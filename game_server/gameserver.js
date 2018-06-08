fd57d804cac7008a721deef987711d9adcd5ad7a
derekahuang <huangda@stanford.edu>
2018-06-08 13:00:54 -0700

    docker documentation

--- game_server/gameserver.js
+++ game_server/gameserver.js
@@ -4,13 +4,21 @@ const Tx = require('ethereumjs-tx');
 const MissionTrackerJson = require('./contracts/MissionTracker.json');
 
 const app = express();
+
+//5001 is the necessary port for communication between Docker image and local machine
 const port = process.env.PORT || 5001;
+
 const providerUrl = 'https://rinkeby.infura.io/N9Txfkh1TNZhoeKXV6Xm';
+
+//Game wallet public and private key
 const gamePublicKey = process.env.public;
 const gamePrivateKey = process.env.private;
 
+/* 
+Testing
 const temp_public = '0x1CE1fa37c955F8f48cf5Cff659eb0885874BBa7b';
 const temp_private = new Buffer('568eb8f8bae05aa41fc9f23eb43daf1043d3b0a0a6994c581be26e521c00c277', 'hex');
+*/
 
 let contractAddress = null;
 let contract = null;
@@ -38,17 +46,19 @@ Promise.all([contractDataPromise, networkIdPromise])
 })
 .catch(console.error);
 
+/*
+	Called when the server receives an HTTP GET method with a complete_checkpoint URL. The server issues 
+	a web3 call to the contract to set a checkpoint to complete, which mints a new token and adds it to 
+	the reviewer's wallet, determined by the reviewerId.
+
+	The transaction is signed with the game private key.
+*/ 
 app.get('/api/complete_checkpoint/:reviewer/:checkpoint', (req, res) => {
     let reviewerId = req.params.reviewer;
     let checkpointId = req.params.checkpoint;
     let encodedABI = contract.methods.setCheckpointComplete(reviewerId, checkpointId).encodeABI();
 
-	console.log("reached here : ");
-	console.log( gamePublicKey);
-
-
-    //web3.eth.getTransactionCount(gamePublicKey, 'pending')
-    web3.eth.getTransactionCount(temp_public, 'pending')
+    web3.eth.getTransactionCount(gamePublicKey, 'pending')
     .then(nonce => {
         let rawTx = {
             from: gamePublicKey,
@@ -60,8 +70,7 @@ app.get('/api/complete_checkpoint/:reviewer/:checkpoint', (req, res) => {
         };
 
         let tx = new Tx(rawTx);
-        tx.sign(temp_private);
-        //tx.sign(gamePrivateKey);
+        tx.sign(gamePrivateKey);
     
         let serializedTx = tx.serialize();
     
@@ -71,13 +80,18 @@ app.get('/api/complete_checkpoint/:reviewer/:checkpoint', (req, res) => {
     })
 });
 
+/*
+	Called when the server receives an HTTP GET method with a add_checkpoint URL. The server issues 
+	a web3 call to the contract to register the existence of a new checkpoint for a game. It is helpful
+	to list all checkpoints to determine what a reviewer is able to complete.
+
+	The transaction is signed with the game private key.
+*/ 
 app.get('/api/add_checkpoint/:checkpoint_name', (req, res) => {
-	console.log("hello");
     let checkpointName = decodeURIComponent(req.params.checkpoint_name);
     let encodedABI = contract.methods.addGameCheckpoint(checkpointName).encodeABI();
 
-    //web3.eth.getTransactionCount(gamePublicKey, 'pending')
-    web3.eth.getTransactionCount(temp_public, 'pending')
+    web3.eth.getTransactionCount(gamePublicKey, 'pending')
     .then(nonce => {
         let rawTx = {
             from: gamePublicKey,
@@ -89,8 +103,7 @@ app.get('/api/add_checkpoint/:checkpoint_name', (req, res) => {
         };
 
         let tx = new Tx(rawTx);
-        tx.sign(temp_private);
-        //tx.sign(gamePrivateKey);
+        tx.sign(gamePrivateKey);
     
         let serializedTx = tx.serialize();
     
@@ -99,4 +112,3 @@ app.get('/api/add_checkpoint/:checkpoint_name', (req, res) => {
         .catch(console.error);
     })
 });
-console.log("end");
